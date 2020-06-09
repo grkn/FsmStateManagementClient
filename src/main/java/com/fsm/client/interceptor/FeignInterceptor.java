effd0aba241c5bf93ae364f4b5d4be0783d1b3e5
gilleez <gilleez@netrd.com.tr>
2020-06-09 22:57:06 +0300

    unused objectmapper removed

--- src/main/java/com/fsm/client/interceptor/FeignInterceptor.java
+++ src/main/java/com/fsm/client/interceptor/FeignInterceptor.java
@@ -1,31 +1,19 @@
 package com.fsm.client.interceptor;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fsm.client.constant.FsmConstants;
 import com.fsm.client.request.TransactionRequestBean;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
 @Component
 public class FeignInterceptor implements RequestInterceptor {
 
 
     private final TransactionRequestBean transactionRequestBean;
-    private ObjectMapper objectMapper;
 
-    public FeignInterceptor(TransactionRequestBean transactionRequestBean, ObjectMapper objectMapper) {
+    public FeignInterceptor(TransactionRequestBean transactionRequestBean) {
         this.transactionRequestBean = transactionRequestBean;
-        this.objectMapper = objectMapper;
-    }
-
-    @PostConstruct
-    public void init() {
-        if (objectMapper == null) {
-            objectMapper = new ObjectMapper();
-        }
     }
 
     @Override
